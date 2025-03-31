e8cad5d9cad5d6c802bae9d43272c758aad50766
Romulo Barreto da Silva <romubah@gmail.com>
2025-03-31 13:14:30 -0300

    feat: add a User class to the model

---
+++ registration_system/model.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, nome, email, idade):
+        self.nome = nome
+        self.email = email
+        self.idade = idade
+    
+    def formatar_dict(self):
+        return {'nome': {self.nome}, 'email': {self.email}, 'idade': {self.idade}} 
